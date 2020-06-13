74e394c861857a010dafc3bedefbcdec703be6be
MoviU <mkachimov@gmail.com>
2020-06-13 10:54:16 +0300

    Edited

--- codes.py
+++ codes.py
@@ -1,2 +1,13 @@
-a = int(input()); b = int(input())
-print('Огорожа:', (a + b) * 2, 'Саджанці:', (a * b) * 4)
+import requests
+f = requests.get('https://ru.wikipedia.org/wiki/Python').text
+def rozdelitel(x):
+	x = x.split('<code>')
+	X = ''
+	for i in x:
+		if '<' in i:
+			x.remove(i)
+	for i in x:
+		X += i
+	return set(X.split('</code>'))
+print(rozdelitel(f))
+
